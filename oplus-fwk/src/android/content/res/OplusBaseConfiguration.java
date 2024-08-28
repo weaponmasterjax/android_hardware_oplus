f00380d7b07eb791f2b2b11f1e5f1e6842ec3446
Pranav Vashi <neobuddy89@gmail.com>
2024-08-28 23:46:59 +0530

    oplus-fwk: Bring in camera bits
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- oplus-fwk/src/android/content/res/OplusBaseConfiguration.java
+++ oplus-fwk/src/android/content/res/OplusBaseConfiguration.java
@@ -3,8 +3,10 @@ package android.content.res;
 import oplus.content.res.OplusExtraConfiguration;
 
 public abstract class OplusBaseConfiguration {
+    
+    public OplusExtraConfiguration mOplusExtraConfiguration = null;
 
     public OplusExtraConfiguration getOplusExtraConfiguration() {
-        return null;
+        return mOplusExtraConfiguration;
     }
 }
